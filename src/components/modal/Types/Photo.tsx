0c9fb7de2a6693f317c55e106809bc1673a7b49a
Daniel Nguyen <dnguyen@sfsu.edu>
2025-05-15 08:54:47 -0700

    Remove Blur background on loading Photo.

--- src/components/modal/Types/Photo.tsx
+++ src/components/modal/Types/Photo.tsx
@@ -1,32 +1,43 @@
-import { Component, createSignal } from "solid-js";
+import { Accessor, Component, createSignal, Setter, Show } from "solid-js";
 import { useManageURLContext } from "../../../context/ManageUrl";
-import { MediaType } from "../../../context/ViewContext";
+import { MediaType, useViewMediaContext } from "../../../context/ViewContext";
 import styles from "./Types.module.css";
+import EditPhoto from "../Editing/EditPhoto";
+
 interface PhotoProps {
   media: MediaType;
+  isVisible: boolean;
+
+  showImageOnly: Accessor<boolean>;
+  setShowImageOnly: Setter<boolean>;
 }
 
 const Photo: Component<PhotoProps> = (props) => {
+  let photoRef: HTMLImageElement;
   const { view } = useManageURLContext();
   const [imgLoading, setImgLoading] = createSignal<boolean>(true);
 
+  const isVisible = () => props.isVisible;
+
+  const { isEditing, setIsEditing } = useViewMediaContext();
+
   return (
-    <div
-      inert
-      class={styles.blurLoad}
-      style={{
-        "background-image": imgLoading() ? `url(${props.media.thumb_path})` : "",
-        "background-size": view.modalObjFit ? "cover" : "contain",
-      }}>
+    <>
       <img
+        ref={(el) => (photoRef = el)}
+        class={styles.imageTag}
         style={{ "object-fit": view.modalObjFit ? "cover" : "contain" }}
         onLoad={() => setImgLoading(false)}
         onError={() => setImgLoading(true)}
         loading="lazy"
-        src={props.media.source_file}
+        src={imgLoading() ? props.media.thumb_path : props.media.source_file}
         alt={`Modal Image`}
       />
-    </div>
+      <Show when={isEditing() && isVisible()}>
+        {/* {isSeeking() && <Spinner />} */}
+        {photoRef! && <EditPhoto photo={photoRef} setIsEditing={setIsEditing} />}
+      </Show>
+    </>
   );
 };
 export default Photo;
